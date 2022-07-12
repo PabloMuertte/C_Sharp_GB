45f98b575db86412a2e5c030c30795147e65d3ee
pablo <kotelnikovpa@yandex.ru>
2022-07-12 17:36:16 +0300

    added first example with bigger digit in dec number

--- Siminar2/Class_work/Program.cs
+++ Siminar2/Class_work/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 1 
+
+//Method find bigger digit
+int FindBiggerDigit(int num)
+{
+    int ed = num % 10;
+    int dec = num / 10;
+    int max;
+
+    if (ed>dec) max = ed;
+    else max = dec;
+
+    return max;
+}
+
+int number = new Random().Next(10,100);
+int digit = FindBiggerDigit(number);
+
+Console.WriteLine($"Bigger digit of {number} is {digit}");

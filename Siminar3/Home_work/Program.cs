d89c5843fb0d33c5eacd50e2cb6062c650a08b4a
pablo <kotelnikovpa@yandex.ru>
2022-07-25 20:53:18 +0300

    Added code of first task for Home work Siminar_3

--- Siminar3/Home_work/Program.cs
+++ Siminar3/Home_work/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Home Work
+//Task 1: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*
+bool Palindrom (int num)
+{
+    int first = num/10000;
+    int last = num % 10;
+    int second = (num/1000)%10;
+    int prelast = (num % 100)/10;
+    if (first ==last && second==prelast) return true;
+    else return false;
+}
+Console.WriteLine();
+Console.WriteLine("Hello, this program can help you to check is your five digit number Palindrom!");
+Console.WriteLine();
+Console.Write ("Please insert your five digit number: ");
+int num =Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+bool result = Palindrom(num);
+if (result == true) Console.WriteLine($"Your number {num} is Palindrom!");
+if (result == false) Console.WriteLine($"Sorry but your number {num} is not a Palindrom!");
+Console.WriteLine();
+Console.WriteLine("Thank you!");
+*/
+
+//
+
+

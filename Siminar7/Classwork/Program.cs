d9cefd9ca5b01c789d65850a52451cab1f0b9435
pablo <kotelnikovpa@yandex.ru>
2022-08-17 13:25:21 +0300

    Added folder for Siminar7

---
+++ Siminar7/Classwork/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+int [,] createRandom2dArray(int rows, int columns,int minValue, int maxValue)
+{
+    int [,] newArray = new int[rows,columns];
+
+    for( int i = 0; i < rows; i++)
+    {
+        for(int j=0; j<columns; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue,maxValue + 1);
+        }
+    }
+    return newArray;
+}
+
+void Show2dArray(int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine ();
+    }
+}
+
+Console.Write("Input number of rows: ");
+int Rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int Columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input minValue: ");
+int MinValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input maxValue: ");
+int MaxValue = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+int [,] array = createRandom2dArray(Rows,Columns,MinValue,MaxValue);
+
+Show2dArray(array);
---
+++ Siminar7/Homework/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+
+double [,] createRandom2dArray(int rows, int columns,double minValue, double maxValue)
+{
+    double [,] newArray = new double[rows,columns];
+
+    for( int i = 0; i < rows; i++)
+    {
+        for(int j=0; j<columns; j++)
+        {
+            //Random rand = new Random();
+            //double number = rand.Next(1,10) + rand.NextDouble(); 
+            newArray[i,j] = new Random().Next() + new Random().NextDouble(minValue, maxValue + 1);
+        }
+    }
+    return newArray;
+}
+
+void Show2dArray(double [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Input number of rows: ");
+int Rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int Columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input minValue: ");
+double MinValue = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input maxValue: ");
+double MaxValue = Convert.ToDouble(Console.ReadLine());
+Console.Clear();
+
+double [,] array = createRandom2dArray(Rows,Columns,MinValue,MaxValue);
+Show2dArray(array);

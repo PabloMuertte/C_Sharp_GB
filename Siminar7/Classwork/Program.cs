26c0e5023497108ae87cfdca39a9c19dcdd7af6c
pablo <kotelnikovpa@yandex.ru>
2022-08-18 15:20:01 +0300

    Added classwork tasks for Siminar 7

--- Siminar7/Classwork/Program.cs
+++ Siminar7/Classwork/Program.cs
@@ -41,9 +41,7 @@ int [,] array = createRandom2dArray(Rows,Columns,MinValue,MaxValue);
 Show2dArray(array);
 */
 
-// Task 3
-//Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
-//и замените эти элементы на их квадраты.
+
 // Task 4
 // Задайте двумерный массив. Найдите сумму элементов, 
 // находящихся на главной диагонали с индексами (0,0); (1;1) и т.д.
@@ -53,7 +51,7 @@ Show2dArray(array);
 //Задайте двумерный массив размера m на n, 
 //каждый элемент в массиве находится по формуле: Aij = i+j. 
 //Выведите полученный массив на экран.
-
+/*
 int [,] createRandom2dArray(int rows, int columns,int minValue, int maxValue)
 {
     int [,] newArray = new int[rows,columns];
@@ -91,4 +89,124 @@ int MaxValue = Convert.ToInt32(Console.ReadLine());
 Console.Clear();
 int [,] array = createRandom2dArray(Rows,Columns,MinValue,MaxValue);
 Show2dArray(array);
+*/
 
+// Task 3
+//Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+//и замените эти элементы на их квадраты.
+/*
+int [,] createRandom2dArray(int rows, int columns,int minValue, int maxValue)
+{
+    int [,] newArray = new int[rows,columns];
+
+    for( int i = 0; i < rows; i++)
+    {
+        for(int j=0; j<columns; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue,maxValue + 1);
+        }
+    }
+    return newArray;
+}
+
+void Show2dArray(int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine ();
+    }
+}
+
+int [,] Search (int [,] array)
+{
+    int [,] newArray1 = new int [0,0];
+
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i%2==0 && j%2==0) array[i,j] = array [i,j] * array [i,j];
+            
+            Console.Write($"{array[i,j]}  ");
+        }
+        Console.WriteLine ();
+    }
+    return newArray1;
+}
+
+Console.Write("Input number of rows: ");
+int Rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int Columns = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Input minValue: ");
+int MinValue =1;// Convert.ToInt32(Console.ReadLine());
+//Console.Write("Input maxValue: ");
+int MaxValue = 10;// Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int [,] Array = createRandom2dArray(Rows,Columns,MinValue,MaxValue);
+Show2dArray(Array);
+Console.WriteLine();
+int [,] array1 = Search (Array);
+Show2dArray(array1);
+*/
+
+
+// Task 4
+// Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали с индексами (0,0); (1;1) и т.д. 
+int [,] createRandom2dArray(int rows, int columns,int minValue, int maxValue)
+{
+    int [,] newArray = new int[rows,columns];
+
+    for( int i = 0; i < rows; i++)
+    {
+        for(int j=0; j<columns; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue,maxValue + 1);
+        }
+    }
+    return newArray;
+}
+
+void Show2dArray(int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine ();
+    }
+}
+
+void Sum (int [,] array)
+{
+    int A = 0;
+    for(int j = 0, k = 0; k < array.GetLength(0); j++, k ++)
+        {
+            A = A + array[k,j];
+            
+            Console.Write($"{A}  ");
+        }
+        Console.WriteLine ();
+}
+
+Console.Write("Input number of rows: ");
+int Rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int Columns = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Input minValue: ");
+int MinValue =1;// Convert.ToInt32(Console.ReadLine());
+//Console.Write("Input maxValue: ");
+int MaxValue = 10;// Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int [,] Array = createRandom2dArray(Rows,Columns,MinValue,MaxValue);
+Show2dArray(Array);
+Console.WriteLine ();
+
+Sum(Array);

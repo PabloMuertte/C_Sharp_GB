751187252d45615c7c1b622156147a4df9657dfa
pablo <kotelnikovpa@yandex.ru>
2022-08-24 20:28:48 +0300

    Added Classwork and Homework for siminar9

---
+++ Siminar9/Classwork/Program.cs
@@ -0,0 +1,84 @@
+﻿// Task 1 Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+/*
+void ShowNums(int n)
+{
+
+    if (n!=1) ShowNums(n-1);
+
+    Console.Write(n + " ");
+}
+
+ShowNums(5);
+/// 1 2 3 4 5
+
+void ShowNums(int n)
+{
+    if (n >= 1)
+    {
+        Console.Write(n + " ");
+        ShowNums(n-1);
+    } 
+}
+
+ShowNums(5);
+
+//5 4 3 2 1
+
+
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int SumOfDigits (int n)
+{
+    if (n != 0) return n % 10 + SumOfDigits(n / 10);
+    return 0;
+}
+
+
+int n = 365;
+int Sum = SumOfDigits(n);
+Console.Write(Sum);
+
+*/
+
+
+// Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от M до N.
+/*
+
+void ShowNums(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write(m + " ");
+        if (m < n) ShowNums(m+1,n);
+    }      
+    if (m > n)
+    {
+        Console.Write( n + " ");
+        if (m>n) ShowNums(m,n+1);       
+    }  
+}
+
+Console.Write("Input your M number: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input your N number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+ShowNums(M,N);
+*/
+
+// Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+
+
+int NumGrade (int A, int B)
+{
+    if (B!=0) return A* NumGrade(A,B-1);
+    return 1;   
+}
+
+Console.Write("Input your A number: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input your B number: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int C = NumGrade(A,B);
+Console.WriteLine(C);
---
+++ Siminar9/Homework/Program.cs
@@ -0,0 +1,76 @@
+﻿
+// Pадача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 5, 6, 7, 8""
+/*
+void ShowNums(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.Write(m + " ");
+        if (m < n) ShowNums(m+1,n);
+    }      
+    if (m > n)
+    {
+        Console.Write( n + " ");
+        if (m>n) ShowNums(m,n+1);       
+    }  
+}
+
+Console.Write("Input your M number: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input your N number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+ShowNums(M,N);
+*/
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int Sum (int m,int n)
+{
+    if (m!=n) return m + Sum(m+1,n);
+    return 0;
+}
+
+Console.Write("Input your M number: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input your N number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+Console.WriteLine(sum = Sum(M,N)+N);
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n
+// m = 3, n = 2 -> A(m,n) = 29
+// m = 2, n = 3 -> A(m,n) = 9
+/*
+int ack( int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if ((m > 0) && (n == 0))
+        {
+            return ack(m - 1, 1);
+        }
+    else if ((m > 0) && (n > 0))
+        {
+            return ack(m - 1, ack(m, n - 1));
+        } 
+    else return n + 1;
+}
+
+Console.Write("Input your M number: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input your N number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Your result is {ack(M, N)}");
+*/
+
+
